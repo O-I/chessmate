8151ef72a177abef5ddd2bb3aa53c0db02c641b1
Rahul Horé <hore.rahul@gmail.com>
2014-12-08 22:18:26 -0500

    Tweaks regexes, fixes whitespace skip

--- lib/chessmate/tokenizer.rb
+++ lib/chessmate/tokenizer.rb
@@ -1,10 +1,10 @@
 module Chessmate
   module PGN
     class Tokenizer        # TODO: tweak regexes to adhere to PGN spec
-      STRING               = /"(?:[^"\\]|\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4}))*"/
-      SYMBOL               = /[a-zA-Z0-9]{1}[\w:\+=\-#]{0,254}/
+      STRING               = /"([[:print:]&&[^\\"]]|\\\\|\\")*"/
+      SYMBOL               = /[a-zA-Z0-9_\w:\+=\-#]+/
       RESULT               = /(1\-0|0\-1|1\/2\-1\/2|\*)/
-      INTEGER              = /\d+/
+      MOVE                 = /\d+\.*/
       NAG                  = /\$\d+/
       WHITESPACE           = /\s+/
 
@@ -14,15 +14,15 @@ module Chessmate
 
       def next_token
         return if @ss.eos?
+        @ss.skip(WHITESPACE)
 
         case
-        when text = @ss.scan(WHITESPACE) ; # ignore whitespace
-        when text = @ss.scan(STRING)     ; [:STRING,  text]
-        when text = @ss.scan(RESULT)     ; [:RESULT,  text]
-        when text = @ss.scan(SYMBOL)     ; [:SYMBOL,  text]
-        when text = @ss.scan(INTEGER)    ; [:INTEGER, text]
-        when text = @ss.scan(NAG)        ; [:NAG,     text]
-        else char = @ss.getch            ; [char,     char]
+        when text = @ss.scan(STRING) ; [:STRING,  text]
+        when text = @ss.scan(RESULT) ; [:RESULT,  text]
+        when text = @ss.scan(MOVE)   ; [:MOVE,    text]
+        when text = @ss.scan(SYMBOL) ; [:SYMBOL,  text]
+        when text = @ss.scan(NAG)    ; [:NAG,     text]
+        else char = @ss.getch        ; [char,     char]
         end
       end
     end
