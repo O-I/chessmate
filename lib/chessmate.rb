4dfecb7500ad2e86b30fb37de311fe9256461efd
Rahul Horé <hore.rahul@gmail.com>
2014-12-10 19:52:04 -0500

    Adds methods for loading PGN input

--- lib/chessmate.rb
+++ lib/chessmate.rb
@@ -1,5 +1,20 @@
-require "chessmate/version"
+require_relative 'chessmate/parser'
+require_relative 'chessmate/tokenizer'
+require_relative 'chessmate/version'
 
 module Chessmate
-  # Your code goes here...
+  module PGN
+    class << self
+      def load_io(input)
+        tok     = Chessmate::PGN::Tokenizer.new input
+        parser  = Chessmate::PGN::Parser.new tok
+        handler = parser.parse
+        handler
+      end
+
+      def load(pgn)
+        load_io StringIO.new pgn
+      end
+    end
+  end
 end
