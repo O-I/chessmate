51d27f184473214eeace821b1b4f9bf87f834541
Rahul Horé <hore.rahul@gmail.com>
2014-12-14 20:43:50 -0500

    Begins testing tokenizer

--- test/tokenizer_test.rb
+++ test/tokenizer_test.rb
@@ -1,11 +1,26 @@
+require 'json'
 require 'minitest/autorun'
 require 'chessmate/tokenizer'
 
 module Chessmate
   module PGN
     class TestTokenizer < MiniTest::Unit::TestCase
-      def test_minitest_runs_properly
-        assert_equal true, true
+
+      [
+        [:STRING, '"F/S Return Match"'],
+        [:STRING, '"Belgrade, Serbia Yugoslavia|JUG"'],
+        [:STRING, '"1992.11.04"'],
+        [:STRING, '"Robert James \"Bobby\" Fischer"'],
+        [:STRING, '"Ą ą Ĳ ĳ \\\\ Љ Щ щ Ӄ"']
+      ].each do |token|
+        define_method("test_#{token}") do
+          tok = new_tokenizer token.last
+          assert_equal token, tok.next_token
+        end
+      end
+
+      def new_tokenizer string
+        Tokenizer.new StringIO.new string
       end
     end
   end
