84defd089d8ab45242a14ed8301456d8d2d5a08b
Rahul Horé <hore.rahul@gmail.com>
2014-12-15 19:43:40 -0500

    Adds tokenizer tests

--- test/tokenizer_test.rb
+++ test/tokenizer_test.rb
@@ -11,7 +11,43 @@ module Chessmate
         [:STRING, '"Belgrade, Serbia Yugoslavia|JUG"'],
         [:STRING, '"1992.11.04"'],
         [:STRING, '"Robert James \"Bobby\" Fischer"'],
-        [:STRING, '"Ą ą Ĳ ĳ \\\\ Љ Щ щ Ӄ"']
+        [:STRING, '"Ą ą Ĳ ĳ \\\\ Љ Щ щ Ӄ"'],
+
+        [:SYMBOL, 'Event'],
+        [:SYMBOL, 'WhiteElo'],
+        [:SYMBOL, 'BlackType'],
+        [:SYMBOL, 'FEN'],
+        [:SYMBOL, 'd4'],
+        [:SYMBOL, 'Nce2'],
+        [:SYMBOL, 'Ra6'],
+        [:SYMBOL, 'B:e5'],
+        [:SYMBOL, 'Be5:'],
+        [:SYMBOL, 'O-O'],
+        [:SYMBOL, 'O-O-O'],
+        [:SYMBOL, 'Qa6xb7#'],
+        [:SYMBOL, 'fxg1=Q+'],
+
+        [:RESULT, '1-0'],
+        [:RESULT, '0-1'],
+        [:RESULT, '1/2-1/2'],
+        [:RESULT, '*'],
+
+        [:MOVE, '1'],
+        [:MOVE, '23'],
+        [:MOVE, '5.'],
+        [:MOVE, '42..'],
+
+        [:NAG, '$1'],
+        [:NAG, '$12'],
+        [:NAG, '$123'],
+
+        ['.', '.'],
+        ['[', '['],
+        [']', ']'],
+        ['(', '('],
+        [')', ')'],
+        ['<', '<'],
+        ['>', '>']
       ].each do |token|
         define_method("test_#{token}") do
           tok = new_tokenizer token.last
